1e6d12645c169746167b0a441796038cbb32a083
Pavel Kulagin <rogalik_3@mail.ru>
2022-12-05 19:49:54 +0300

    Выполнил 4 модуль

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+  let result = friends.map((item) => {
+    return `<li>${item.firstName} ${item.lastName}</li>`;
+  }).join("\n");
+  ul.innerHTML = `${result}`;
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,14 @@
 function highlight(table) {
-  // ваш код...
-}
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    if (row.cells[3].dataset.available === "true") {
+      row.classList.add("available"); 
+    } else if (row.cells[3].dataset.available === "false") {
+      row.classList.add("unavailable"); 
+    } else {
+      row.hidden = true;
+    }
+    (row.cells[2].innerHTML === "m") ? row.classList.add("male") : row.classList.add("female");
+    if (Number(row.cells[1].innerHTML) < 18) row.setAttribute('style', "text-decoration: line-through");
+  }
+} 
